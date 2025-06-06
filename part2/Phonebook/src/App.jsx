d2dae4d5000af357402498b6e81b7b04e1b03367
Alice Li Maunumäki <aliceli@Alices-MacBook-Pro.local>
2025-06-06 16:09:22 +0300

    2.6 completed

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -6,29 +6,36 @@ const App = (props) => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleAddName = (event) => {setNewName(event.target.value)}
+
   const addName = (event) => {
 	event.preventDefault()
 	console.log('button clicked')
 	console.log(newName)
-	setNewName(event.target.value);
+	const newPerson = {name: newName}
+	setPersons(persons.concat(newPerson))
+	setNewName('')
   }
-
-  const handlePersons = ()
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          	name: <input value={handlePersons}/>
+          	name: <input value={newName} onChange={handleAddName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
 
+      <h2>Numbers</h2>
+		<div>
+			{persons.map((person, index) => (
+			<p key={index}>{person.name}</p>
+			))}
     </div>
+	</div>
   )
 }
 
